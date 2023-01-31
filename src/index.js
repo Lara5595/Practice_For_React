76e08eef0daf5c2b230b17b13dd97b78d2d1264f
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2023-01-31 14:48:21 -0600

    did jsx css and im on jsx javascript

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 // This is how we import css we put . because we have to find the file path
 import './booktutorial.css'
 
+
+
+
 const BookList = () => {
     // If we want to style we use a className
     return <section className="booklist">
@@ -16,25 +19,78 @@ const BookList = () => {
 
 const Book = () => {
     return <article className="book">
-        <Image/>
-        <Title/>
-        <Author/>
+        <img src=" https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg"/>
+        <h2>It Ends with Us: A Novel</h2>
+        <h4>Colleen Hoover</h4>
     </article>
 }
-
-
-// We created components so we can use em inside the books
-const Image = () => <img src=" https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg"/>;
-const Title = () => <h2>It Ends with Us: A Novel</h2>;
-
-    // You can also make a component like this ex is author
-const Author = () => {
-   return <h4>Colleen Hoover</h4>;
-}
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList />);
+
+
+
+
+
+
+// Other way to create is on the notes below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////// NOTES ////////////////////////////////////////////
+// const BookList = () => {
+//     // If we want to style we use a className
+//     return <section className="booklist">
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//     </section>
+// }
+//
+// const Book = () => {
+//     return <article className="book">
+//         <Image/>
+//         <Title/>
+//         <Author/>
+//     </article>
+// }
+//
+//
+// // We created components, so we can use em inside the books
+// const Image = () => <img src=" https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg"/>;
+//
+// // this is how you would import a image if you had a img folder
+// // const Image = () => <img src="/images/lessonsInChemistry.webp"/>;
+//
+//
+// // You can also create a variable and pass it as a style
+// const inlineHeadingStyles = {
+//     color: 'pink'
+// }
+//
+// const Title = () => <h2 style={inlineHeadingStyles}>It Ends with Us: A Novel</h2>;
+//
+//     // You can also make a component like this ex is author. Inline css takes priority over css file.
+// const Author = () => {
+//    //  You can also use inline css
+//    return <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop:'0.5rem' }}>Colleen Hoover</h4>;
+// }
